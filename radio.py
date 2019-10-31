0316a4afd87e066dd476e4db9c0c3755ec416669
Andrew Reilley <areill1337@gmail.com>
2019-10-30 23:50:49 -0400

    Improved test interface script

--- radio.py
+++ radio.py
@@ -6,25 +6,53 @@ class radioPacket():
     def __init__(self, data):
         self.type = data[0]
         self.checksum = data[1]
-        self.seqNum = data[2:4]
+        self.seqNum = self.interpret16Bit(data[2:4])
         self.payload = data[4:12]
 
-    def interpretADCReading(self, startIndex):
-        low = self.payload[startIndex]
-        mid = self.payload[startIndex + 1]
-        high = self.payload[startIndex + 2]
+    def interpret24Bit(self, data):
+        low = data[0]
+        mid = data[1]
+        high = data[2]
         return (low) + (mid * (2**8)) + (high * (2**16))
+
+    def interpret16Bit(self, data):
+        low = data[0]
+        high = data[1]
+        return (low) + (high * (2**8))
 
 
 class setupPacket(radioPacket):
     def __init__(self, data):
         super().__init__(data)
-        self.force = self.interpretADCReading(0)
-        self.pressure = self.interpretADCReading(3)
+        self.force = self.interpret24Bit(self.payload[0:])
+        self.pressure = self.interpret24Bit(self.payload[3:])
         self.continuity = bool(self.payload[6])
 
     def __str__(self):
         out = "Force: {}, Pressure: {}, Continuity: {}".format(self.force, self.pressure, self.continuity)
+        return out
+
+
+class errorPacket(radioPacket):
+    def __init__(self, data):
+        super().__init__(data)
+        self.storageError = self.payload[0]
+        self.adcError = self.payload[1]
+
+    def __str__(self):
+        out = "Storage: {}, ADC: {}".format(self.storageError, self.adcError)
+        return out
+
+
+class resultPacket(radioPacket):
+    def __init__(self, data):
+        super().__init__(data)
+        self.time = self.interpret16Bit(self.payload)
+        self.force = self.interpret24Bit(self.payload[2:])
+        self.pressure = self.interpret24Bit(self.payload[5:])
+
+    def __str__(self):
+        out = "#: {}, Time: {}, Force: {}, Pressure: {}".format(self.seqNum, self.time, self.force, self.pressure)
         return out
 
 
@@ -33,10 +61,18 @@ class RadioManager():
     PREAMBLE = [0xAA, 0xBB]
     ESCAPE = 0x11
 
+    PACKET_TYPE_MAP = {
+        0: setupPacket,
+        1: errorPacket,
+        2: resultPacket
+    }
+
     def __init__(self, port):
         self.toSend = []
         self.port = port
         self.serialThread = Thread(target=self._serialThread)
+
+        self.results = {}
 
     @staticmethod
     def checkPacket(packet):
@@ -50,6 +86,14 @@ class RadioManager():
         pack = [packetType, 0, seqNumHigh, seqNumLow] + payload
         pack[1] = (256 - sum(pack)) % 256
         self.toSend.append(bytearray(RadioManager.PREAMBLE + pack))
+
+    def buildPacket(self, packetData):
+        packetCons = RadioManager.PACKET_TYPE_MAP[packetData[0]]
+        pack = packetCons(packetData)
+        if type(pack) is resultPacket:
+            self.results[pack.seqNum] = pack
+        else:
+            print(pack)
 
     def _serialThread(self):
         with serial.Serial(self.port, 9600) as serport:
@@ -69,8 +113,7 @@ class RadioManager():
                     elif inPacket and (b != RadioManager.ESCAPE or escape):
                         packetBuff.append(b)
                         if self.checkPacket(packetBuff):
-                            pack = setupPacket(packetBuff)
-                            print(pack)
+                            self.buildPacket(packetBuff)
                             inPacket = False
                     escape = b == RadioManager.ESCAPE
 
@@ -84,5 +127,34 @@ class RadioManager():
 
 rm = RadioManager('/dev/ttyUSB0')
 rm.run()
+
 time.sleep(5)
-rm.sendPacket(128, 0, [0, 1, 2, 3, 4, 5, 6, 7])
+for i in range(0, 5):
+    rm.sendPacket(128, 0, [0x00, 0x1B, 0, 0, 0, 0, 0, 0])
+    time.sleep(0.05)
+
+
+def animate(i):
+    global lastLen
+    if len(rm.results) > 10 and len(rm.results) > lastLen:
+        lastLen = len(rm.results)
+        x = []
+        y = []
+        recv = list(rm.results.keys())
+        recv.sort()
+        for p in recv:
+            x.append(rm.results[p].time)
+            y.append((rm.results[p].force - 110000) * 70 / 390000)
+        ax.clear()
+        plt.title('{}/1000'.format(len(rm.results)))
+        ax.plot(x, y)
+
+import matplotlib.pyplot as plt
+import matplotlib.animation as animation
+
+lastLen = 0
+
+fig = plt.figure()
+ax = fig.add_subplot(1,1,1)
+ani = animation.FuncAnimation(fig, animate, interval=250)
+plt.show()
